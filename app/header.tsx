8ed991f7b3d3c4631c763d82c14a3c468de3b897
Mohd Suhairi Ahmad Soobni <mohdsuhairi@gmail.com>
2024-06-25 18:58:33 +0800

    Add logo on header

--- app/header.tsx
+++ app/header.tsx
@@ -1,9 +1,32 @@
 "use client";
 
+import { ModeToggle } from "@/components/ui/mode-toogle";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { Authenticated, Unauthenticated } from "convex/react";
+import Image from "next/image";
+
 export function Header() {
     return (
-        <div>
-            HEADER
+        <div className="bg-slate-300 py-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="flex gap-4 items-center text-2xl">
+                <Image src="/eceos_logo.png" width={100 } height={40} className="rounded" alt="eceos logo" />
+                    eQHub
+                </div>
+                <div>
+                    <Unauthenticated>
+                    <SignInButton />
+                    </Unauthenticated>
+                    <Authenticated>
+                        <div className="flex gap-4 items-center">
+                            <ModeToggle />
+
+                            <UserButton />
+                        </div>
+                        
+                    </Authenticated>
+                </div>
+            </div>
         </div>
     );
 }
